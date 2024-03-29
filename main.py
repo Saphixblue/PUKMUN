8adbe82e19ce23768058035346401d66962d6fb8
nawfelinho <nawfel.rais@gmail.com>
2024-03-29 17:48:30 +0100

    test

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame
 import sys
 
-# Define constants
-GAME_WIDTH, GAME_HEIGHT = 28, 31
-GAME_SPEED = 10
+
 # Dimensions de la fenêtre
 """
 largeur_fenetre = 28 * 16  # Taille d'une case (16 pixels) multipliée par le nombre de cases en largeur
@@ -19,23 +17,17 @@ BLANC = (255, 255, 255)
 # Initialize pygame
 pygame.init()
 
-"""
-# Create the game window
-screen = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT), pygame.FULLSCREEN)
-
-"""
-
 # Création de la fenêtre
 fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre), pygame.FULLSCREEN)
 #fenetre = pygame.display.set_mode((largeur_fenetre, hauteur_fenetre))
 pygame.display.set_caption('Pacman')
 
 # Chargement des images
-Fantome_ivre = pygame.image.load('Fantome_ivre_test.png').convert_alpha()
+#Fantome_ivre = pygame.image.load('Fantome_ivre_test.png').convert_alpha()
 
 # Define the starting position of Pacman
-Fantome_ivre_x = largeur_fenetre // 2
-Fantome_ivre_y = hauteur_fenetre // 2
+#Fantome_ivre_x = largeur_fenetre // 2
+#Fantome_ivre_y = hauteur_fenetre // 2
 
 # Carte de jeu (exemple)
 carte = [
@@ -78,8 +70,8 @@ def dessiner_carte():
                 pygame.draw.rect(fenetre, BLEU, (x * 16, y * 16, 16, 16))
 
 # Game loop
-running = True
-while running:
+Jeu = True
+while Jeu:
     # Handle user input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -89,32 +81,14 @@ while running:
 
     # Draw on the screen
     fenetre.fill(NOIR)
-    # Add code here to draw the Pacman character, ghosts, and maze
+
+    # Code pour dessiner Pacman les ghosts, et la map
 
     # Dessiner la carte
     #dessiner_carte()
 
-    # Update the screen
+    # Mettre à jour les modifs de la boucle sur l'écran
     pygame.display.update()
-
-# Boucle principale
-while True:
-    # Gestion des événements
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    # Effacer l'écran
-    fenetre.fill(NOIR)
-
-    # Dessiner la carte
-    dessiner_carte()
-
-    # Mettre à jour l'affichage
-    pygame.display.flip()
-
-
 
 
 # Exit the game
