e4540fb045fe9c780b7d65aacd1edba32440f830
nawfelinho <nawfel.rais@gmail.com>
2024-04-25 13:53:40 +0200

    création de la map grâce à la classe map de map.py. Création d'une map de 23x20 cases sur du 800x600 pixels. Une case fait du 30x30 px. Pacman est réglé à 30x30px. Méthodes draw_rectangle et draw_angles pour créatio d'obstacles dans la classe Map.

--- Map.py
+++ Map.py
@@ -1,31 +1,38 @@
-map = [[0 for _ in range(20)] for _ in range(23)]
+# map.py
+import pygame as pg
 
+class Map:
+    def __init__(self, window_size, cell_size):
+        self.window_size = window_size
+        self.cell_size = cell_size
+        self.rows = window_size[1] // cell_size
+        self.cols = window_size[0] // cell_size
 
-def DrawAngle(x, y, longeur, positionAngle, longeurAngle=1, directionAngle="bas"):
-    """Dessine une forme de T avec directionAngle haut ou bas"""
-    for i in range(longeur):
-        map[x + i][y] = 3
-    for j in range(longeurAngle):
-        if directionAngle == "haut":
-            direction = -j
-        else:
-            direction = j
-        map[x + positionAngle - 1][y + direction + 1] = 3
+        # Création de la carte avec des cases vides par défaut
+        self.map_data = [[0 for _ in range(self.cols)] for _ in range(self.rows)]
 
+    def draw_map(self, screen):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.map_data[i][j] == 3:
+                    # Dessiner un mur (obstacle)
+                    obstacle_rect = pg.Rect(j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size)
+                    pg.draw.rect(screen, (255, 255, 255), obstacle_rect)
 
-def DrawRectangle(x, y, longeur, largeur=1):
-    for i in range(longeur):
-        for j in range(largeur):
-            map[x + i][y + j] = 3
+    def draw_rectangle_obstacle(self, x, y, longueur, largeur):
+        for i in range(longueur):
+            for j in range(largeur):
+                if 0 <= x + i < self.rows and 0 <= y + j < self.cols:
+                    self.map_data[x + i][y + j] = 3
 
-
-def InitMap():
-    DrawAngle(8, 8, 3, 1, 2)
-    DrawAngle(9, 10, 6, 6, 2)
-    DrawRectangle(12, 8, 2)
-    DrawRectangle(0, 8, 5, 2)
-    DrawRectangle(0, 11, 5, 2)
-    DrawRectangle(18, 8, 5, 2)
-    DrawRectangle(18, 11, 5, 2)
-
-
+    def draw_angle_obstacle(self, x, y, longueur, position_angle, longueur_angle, direction_angle):
+        for i in range(longueur):
+            if 0 <= x + i < self.rows and 0 <= y < self.cols:
+                self.map_data[x + i][y] = 3
+        for j in range(longueur_angle):
+            if direction_angle == "haut":
+                direction = -j
+            else:
+                direction = j
+            if 0 <= x + position_angle - 1 < self.rows and 0 <= y + direction + 1 < self.cols:
+                self.map_data[x + position_angle - 1][y + direction + 1] = 3
--- game.py
+++ game.py
@@ -1,30 +1,35 @@
 # game.py
+
 import pygame
 import sys
 import os
+from map import Map
 
 # Initialisation de Pygame
 pygame.init()
 
 # Paramètres de la fenêtre
 WINDOW_SIZE = (800, 600)
-BLACK = (0, 0, 0)
+CELL_SIZE = 30  # Définir la taille des cellules
 
 # Créer la fenêtre
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Pac-Man")
 
-# Déterminer le chemin vers le dossier des images
-current_path = os.path.dirname(__file__)  # Chemin du répertoire actuel du fichier
-image_path = os.path.join(current_path, 'images')  # Chemin vers le dossier des images
+# Instance de la classe Map
+game_map = Map(WINDOW_SIZE, CELL_SIZE)
 
 # Charger l'image de Pac-Man et redimensionner
+current_path = os.path.dirname(__file__)  # Chemin du répertoire actuel du fichier
+image_path = os.path.join(current_path, 'images')  # Chemin vers le dossier des images
 pacman_image_path = os.path.join(image_path, 'pacman.png')
 Pacman = pygame.image.load(pacman_image_path).convert()
-Pacman = pygame.transform.scale(Pacman, (40, 40))  # Redimensionner à la taille souhaitée
+Pacman = pygame.transform.scale(Pacman, (30, 30))  # Redimensionner à la taille souhaitée
 
 # Position de départ de Pac-Man
 pacman_position = [300, 300]
+
+# Vitesse de déplacement de Pac-Man
 pacman_speed = 5
 
 # Direction de départ de Pac-Man (vers la droite)
@@ -35,9 +40,23 @@ def quit_game():
     pygame.quit()
     sys.exit()
 
+# Vérifier si Pac-Man entre en collision avec un obstacle
+def check_collision(x, y):
+    cell_x = x // CELL_SIZE
+    cell_y = y // CELL_SIZE
+
+    # Vérifier si les coordonnées sont dans les limites de la carte
+    if cell_x < 0 or cell_x >= len(game_map.map_data[0]) or cell_y < 0 or cell_y >= len(game_map.map_data):
+        return False
+
+    if game_map.map_data[cell_y][cell_x] == 3:
+        return True
+    return False
+
 # Boucle principale du jeu
 def game():
-    global pacman_direction  # Déclaration de la variable globale car elle est définie en dehors de la fonctions
+    global pacman_direction
+    global pacman_position
 
     running = True
     while running:
@@ -45,7 +64,6 @@ def game():
             if event.type == pygame.QUIT:
                 quit_game()
 
-            # Gérer les événements des touches directionnelles
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     pacman_direction = "LEFT"
@@ -56,102 +74,49 @@ def game():
                 elif event.key == pygame.K_DOWN:
                     pacman_direction = "DOWN"
 
-
-        # Déplacer Pac-Man dans la direction actuelle
+        next_position = pacman_position.copy()
         if pacman_direction == "LEFT":
-            pacman_position[0] -= pacman_speed
+            next_position[0] -= pacman_speed
         elif pacman_direction == "RIGHT":
-            pacman_position[0] += pacman_speed
+            next_position[0] += pacman_speed
         elif pacman_direction == "UP":
-            pacman_position[1] -= pacman_speed
+            next_position[1] -= pacman_speed
         elif pacman_direction == "DOWN":
-            pacman_position[1] += pacman_speed
+            next_position[1] += pacman_speed
 
-        # Limitation de Pac-Man dans la fenêtre
+        # Vérifier si Pac-Man entre en collision avec un obstacle
+        if not check_collision(next_position[0], next_position[1]) \
+                and not check_collision(next_position[0] + Pacman.get_width() - 1, next_position[1]) \
+                and not check_collision(next_position[0], next_position[1] + Pacman.get_height() - 1) \
+                and not check_collision(next_position[0] + Pacman.get_width() - 1, next_position[1] + Pacman.get_height() - 1):
+            pacman_position = next_position
+
+        # Limiter Pac-Man dans la fenêtre
         pacman_position[0] = max(0, min(pacman_position[0], WINDOW_SIZE[0] - Pacman.get_width()))
         pacman_position[1] = max(0, min(pacman_position[1], WINDOW_SIZE[1] - Pacman.get_height()))
 
-        # Affichage de Pac-Man et de la carte
-        screen.fill(BLACK)
+        # Effacer l'écran
+        screen.fill((0, 0, 0))
+
+        # Dessiner la carte
+        game_map.draw_map(screen)
+
+        # Dessiner Pac-Man
         screen.blit(Pacman, pacman_position)
+
+        # Mettre à jour l'affichage
         pygame.display.flip()
 
-        # Limiter le taux de rafraîchissement de l'écran
+        # Limiter le nombre d'images par seconde
         pygame.time.Clock().tick(60)
+
+# Mettre en place un obstacle sur la carte
+game_map.draw_rectangle_obstacle(5, 10, 5,1)  # Dessine un rectangle à la case (10;10) longueur 5, largeur 1
+game_map.draw_angle_obstacle(2,2,3,2,1,"bas") # dessin angle en (2,2); longueur 3; position angle 2, longueurangle 1; position "bas"
+game_map.draw_angle_obstacle(10,20,3,2,3,"haut") # dessin angle en (2,2); longueur 3; position angle 2, longueurangle 1; position "bas"
+#game_map.draw_rectangle_obstacle(0, 0, 30,30)  # Dessine un rectangle sur toute la map
+
+
 
 if __name__ == "__main__":
     game()
-
-
-'''
-import main  # Importer main.py pour lancer le jeu principal
-import menu  # Importer game.py pour lancer le jeu principal
-import pygame
-import sys
-
-# Initialisation de Pygame
-pygame.init()
-
-# Paramètres de la fenêtre
-WINDOW_SIZE = (600, 600)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-
-# Créer la fenêtre
-screen = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption("Pac-Man")
-
-# Charger l'image de Pac-Man et redimensionner
-Pacman = pygame.image.load('pacman.png').convert()
-Pacman = pygame.transform.scale(Pacman, (40, 40))  # Redimensionner à la taille souhaitée
-
-# Position de départ de Pac-Man
-pacman_position = [300, 300]
-pacman_speed = 5
-
-# Direction de départ de Pac-Man (vers la droite)
-pacman_direction = "RIGHT"
-
-# Boucle principale
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        # Gérer les événements des touches directionnelles
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                pacman_direction = "LEFT"
-            elif event.key == pygame.K_RIGHT:
-                pacman_direction = "RIGHT"
-            elif event.key == pygame.K_UP:
-                pacman_direction = "UP"
-            elif event.key == pygame.K_DOWN:
-                pacman_direction = "DOWN"
-
-    # Déplacer Pac-Man dans la direction actuelle
-    if pacman_direction == "LEFT":
-        pacman_position[0] -= pacman_speed
-    elif pacman_direction == "RIGHT":
-        pacman_position[0] += pacman_speed
-    elif pacman_direction == "UP":
-        pacman_position[1] -= pacman_speed
-    elif pacman_direction == "DOWN":
-        pacman_position[1] += pacman_speed
-
-    # Limitation de Pac-Man dans la fenêtre
-    pacman_position[0] = max(0, min(pacman_position[0], WINDOW_SIZE[0] - Pacman.get_width()))
-    pacman_position[1] = max(0, min(pacman_position[1], WINDOW_SIZE[1] - Pacman.get_height()))
-
-    # Affichage de Pac-Man et de la carte
-    screen.fill(BLACK)
-    screen.blit(Pacman, pacman_position)
-    pygame.display.flip()
-
-    # Limiter le taux de rafraîchissement de l'écran
-    pygame.time.Clock().tick(60)
-
-pygame.quit()
-sys.exit()
-'''
